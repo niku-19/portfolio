e7d2d81ebf8526197dbfdd01f3299403a5539216
niku-19 <nikhil@Nikhils-MacBook-Air.local>
2024-12-05 14:40:30 +0530

    [chore] : fix build

--- src/components/magicui/blur-fade.tsx
+++ src/components/magicui/blur-fade.tsx
@@ -29,7 +29,7 @@ const BlurFade = ({
   blur = "6px",
 }: BlurFadeProps) => {
   const ref = useRef(null);
-  const inViewResult = useInView(ref, { once: true, margin: inViewMargin });
+  const inViewResult = useInView(ref, { once: true, margin: inViewMargin as any });
   const isInView = !inView || inViewResult;
   const defaultVariants: Variants = {
     hidden: { y: yOffset, opacity: 0, filter: `blur(${blur})` },
@@ -56,5 +56,4 @@ const BlurFade = ({
     </AnimatePresence>
   );
 };
-
 export default BlurFade;
